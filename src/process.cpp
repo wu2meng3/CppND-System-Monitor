47ff7eb7542c849683f1dbfdbde565b037532bf9
Meng Wu <wu1meng2@berkeley.edu>
2022-06-20 14:35:26 +0000

    Add cmd_in to process

--- src/process.cpp
+++ src/process.cpp
@@ -10,8 +10,8 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-Process::Process(int pid_in, const string& user_in)
-: pid_(pid_in), user_(user_in)
+Process::Process(int pid_in, const string& user_in, const string& cmd_in)
+: pid_(pid_in), user_(user_in), cmd_(cmd_in)
 {
 }
 
@@ -32,8 +32,7 @@ float Process::CpuUtilization() { return 0; }
 // TODO: Return the command that generated this process
 string Process::Command()
 { 
-    // return cmd_;
-    return "cd";
+    return cmd_;
 }
 
 // TODO: Return this process's memory utilization
@@ -60,6 +59,6 @@ long int Process::UpTime()
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a[[maybe_unused]]) const 
 { 
-    //return this->Pid() < a.Pid();
+    //return this->CpuUtilization() < a.CpuUtilization();
     return true;
 }
