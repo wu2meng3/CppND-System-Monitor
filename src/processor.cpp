fca1f75b92f03e91946a60dcff4dc5bc47bb6b18
Meng Wu <wu1meng2@berkeley.edu>
2022-06-21 03:00:21 +0000

    Refactor Processor::Utilization()

--- src/processor.cpp
+++ src/processor.cpp
@@ -6,13 +6,13 @@ float Processor::Utilization()
 { 
     long idle_so_far = LinuxParser::IdleJiffies();
     long active_so_far = LinuxParser::ActiveJiffies();
-    long idle_now = idle_so_far - prev_idle_so_far;
-    long active_now = active_so_far - prev_active_so_far;
-    long total_now = idle_now + active_now;
-    if (total_now <= 0) throw std::runtime_error("Invalid total jiffies to calculate cpu utilization.");
+    long idle_jiffies = idle_so_far - prev_idle_so_far;
+    long active_jiffies = active_so_far - prev_active_so_far;
+    long total_jiffies = idle_jiffies + active_jiffies;
+    if (total_jiffies <= 0) throw std::runtime_error("Invalid total jiffies to calculate cpu utilization.");
 
     prev_idle_so_far = idle_so_far;
     prev_active_so_far = active_so_far;
 
-    return (float) active_now / (float) total_now;
+    return (float) active_jiffies / (float) total_jiffies;
 }
